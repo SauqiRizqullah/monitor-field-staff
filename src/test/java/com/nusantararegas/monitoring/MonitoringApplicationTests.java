deae6ade8cb66fcc075058021a04388829c30dfd
SauqiRizqullah <sauqi.risqullah@gmail.com>
2025-11-25 17:53:47 +0700

    [INIT] Initiate Project:

---
+++ src/test/java/com/nusantararegas/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nusantararegas.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
